9a2978c3ec478359947cb97e430e23fdbac45377
miraj98hossain <miraj09hossain@gmail.com>
2025-02-07 16:42:01 +0600

    data and domain folder location changed

--- lib/data/repositories/app_repository_impl.dart
+++ lib/data/repositories/app_repository_impl.dart
@@ -1,6 +1,6 @@
-import 'package:popular_git_repos/app/data/data_sources/app_local_data_source.dart';
-import 'package:popular_git_repos/app/data/data_sources/app_remote_data_source.dart';
-import 'package:popular_git_repos/app/domain/repositories/app_repository.dart';
+import 'package:popular_git_repos/data/data_sources/app_local_data_source.dart';
+import 'package:popular_git_repos/data/data_sources/app_remote_data_source.dart';
+import 'package:popular_git_repos/domain/repositories/app_repository.dart';
 
 class AppRepositoryImpl implements AppRepository {
   final AppRemoteDataSource appRemoteDataSource;
