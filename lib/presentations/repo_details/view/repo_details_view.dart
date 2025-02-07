cafeb825eeb0b146bf3b763b15b8bad3cf15c719
miraj98hossain <miraj09hossain@gmail.com>
2025-02-07 22:51:17 +0600

    Refactor repo_details_view.dart to enable the scroll for long desc repo.

--- lib/presentations/repo_details/view/repo_details_view.dart
+++ lib/presentations/repo_details/view/repo_details_view.dart
@@ -33,88 +33,90 @@ class _RepoDetailsViewState extends State<RepoDetailsView> {
           ),
         ),
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 20,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  image: DecorationImage(
-                    image:
-                        NetworkImage(widget.repository.owner?.avatarUrl ?? ''),
-                    fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          widget.repository.owner?.avatarUrl ?? ''),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                widget.repository.owner?.login ?? '',
-                style: lightTextTheme.bodyMedium!.copyWith(
-                  fontSize: 20,
+              const SizedBox(height: 10),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  widget.repository.owner?.login ?? '',
+                  style: lightTextTheme.bodyMedium!.copyWith(
+                    fontSize: 20,
+                    color: appTheme.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                widget.repository.description ?? '',
+                style: lightTextTheme.bodySmall!.copyWith(
                   color: appTheme.white,
                   fontWeight: FontWeight.w600,
                 ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              widget.repository.description ?? '',
-              style: lightTextTheme.bodySmall!.copyWith(
-                color: appTheme.white,
-                fontWeight: FontWeight.w600,
+              Text(
+                widget.repository.pushedAt != null
+                    ? 'Last Updated: ${DateTime.parse(widget.repository.pushedAt!).toFormatedString("mm-dd-yyyy hh:mm a")}'
+                    : '',
+                style: lightTextTheme.bodySmall!.copyWith(
+                  color: appTheme.white,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-            Text(
-              widget.repository.pushedAt != null
-                  ? 'Last Updated: ${DateTime.parse(widget.repository.pushedAt!).toFormatedString("mm-dd-yyyy hh:mm a")}'
-                  : '',
-              style: lightTextTheme.bodySmall!.copyWith(
-                color: appTheme.white,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Wrap(
-              spacing: 5,
-              runSpacing: 10,
-              children: [
-                ...List.generate(widget.repository.topics?.length ?? 0,
-                    (index) {
-                  var topic = widget.repository.topics?[index] ?? "";
-                  return Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 5,
-                    ),
-                    decoration: BoxDecoration(
-                      color: appTheme.white,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Text(
-                      topic,
-                      style: lightTextTheme.bodySmall!.copyWith(
-                        color: appTheme.black,
-                        fontWeight: FontWeight.w600,
+              const SizedBox(height: 10),
+              Wrap(
+                spacing: 5,
+                runSpacing: 10,
+                children: [
+                  ...List.generate(widget.repository.topics?.length ?? 0,
+                      (index) {
+                    var topic = widget.repository.topics?[index] ?? "";
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
                       ),
-                    ),
-                  );
-                })
-              ],
-            )
-          ],
+                      decoration: BoxDecoration(
+                        color: appTheme.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        topic,
+                        style: lightTextTheme.bodySmall!.copyWith(
+                          color: appTheme.black,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    );
+                  })
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
