cdcffcab64e42053909487ecab7fafb36ffca166
miraj98hossain <miraj09hossain@gmail.com>
2025-02-07 16:42:36 +0600

    refactor: Update import paths for data and domain folders

--- lib/dependency_injector/di_container.dart
+++ lib/dependency_injector/di_container.dart
@@ -1,8 +1,8 @@
 import 'package:get_it/get_it.dart';
-import 'package:popular_git_repos/app/data/data_sources/app_local_data_source.dart';
-import 'package:popular_git_repos/app/data/data_sources/app_remote_data_source.dart';
-import 'package:popular_git_repos/app/data/repositories/app_repository_impl.dart';
-import 'package:popular_git_repos/app/domain/repositories/app_repository.dart';
+import 'package:popular_git_repos/data/data_sources/app_local_data_source.dart';
+import 'package:popular_git_repos/data/data_sources/app_remote_data_source.dart';
+import 'package:popular_git_repos/data/repositories/app_repository_impl.dart';
+import 'package:popular_git_repos/domain/repositories/app_repository.dart';
 import 'package:popular_git_repos/core/app_config.dart';
 import 'package:popular_git_repos/core/services/local_database.dart';
 import 'package:sqflite/sqflite.dart';
